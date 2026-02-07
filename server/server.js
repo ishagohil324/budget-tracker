657dcb371d537239cb352be0dd64fdd6afe69d72
ISHA GOHIL <ishagohil324@gmail.com>
2026-02-07 13:29:01 +0530

    save pending changes

--- server/server.js
+++ server/server.js
@@ -235,7 +235,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || '*',
+  origin: process.env.CLIENT_URL || 'https://budget-tracker-ten-iota.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
